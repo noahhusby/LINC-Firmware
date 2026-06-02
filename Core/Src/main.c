46ce089907104788d662d63061bac24cb36baf02
noahhusby <32528627+noahhusby@users.noreply.github.com>
2026-06-01 21:33:31 -0500

    Set the GPIB DIO to be inputs by default to avoid short state on startup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -308,26 +308,23 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, DIO1_Pin|DIO2_Pin|DIO3_Pin|DIO4_Pin
-                          |DIO5_Pin|DIO6_Pin|DIO7_Pin|DIO8_Pin
-                          |iATN_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIB_EN_Pin|GPIB_DIR_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, iREN_Pin|iEOI_Pin|iDAV_Pin|iNRFD_Pin
                           |iNDAC_Pin|iIFC_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(iATN_GPIO_Port, iATN_Pin, GPIO_PIN_RESET);
+
   /*Configure GPIO pins : DIO1_Pin DIO2_Pin DIO3_Pin DIO4_Pin
                            DIO5_Pin DIO6_Pin DIO7_Pin DIO8_Pin
-                           iATN_Pin */
+                           NRFD_SENSE_Pin */
   GPIO_InitStruct.Pin = DIO1_Pin|DIO2_Pin|DIO3_Pin|DIO4_Pin
                           |DIO5_Pin|DIO6_Pin|DIO7_Pin|DIO8_Pin
-                          |iATN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+                          |NRFD_SENSE_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : GPIB_EN_Pin GPIB_DIR_Pin */
@@ -352,11 +349,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : NRFD_SENSE_Pin */
-  GPIO_InitStruct.Pin = NRFD_SENSE_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  /*Configure GPIO pin : iATN_Pin */
+  GPIO_InitStruct.Pin = iATN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(NRFD_SENSE_GPIO_Port, &GPIO_InitStruct);
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(iATN_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
