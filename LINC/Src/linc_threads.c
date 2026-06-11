6de0e5f70329c5ec5cd08d7a227f19a480857244
Noah Husby <32528627+noahhusby@users.noreply.github.com>
2026-06-11 12:38:50 -0500

    Add basic pre-commit hook (#1)

--- LINC/Src/linc_threads.c
+++ LINC/Src/linc_threads.c
@@ -12,17 +12,10 @@
 static TX_THREAD linc_status_led_thread;
 static ULONG linc_status_led_stack[LINC_STATUS_LED_STACK_SIZE / sizeof(ULONG)];
 
-UINT linc_threads_create(void) {
-    const UINT status = tx_thread_create(
-        &linc_status_led_thread,
-        "LINC Status LED",
-        linc_status_led_thread_entry,
-        0,
-        linc_status_led_stack,
-        sizeof(linc_status_led_stack),
-        LINC_STATUS_LED_PRIORITY,
-        LINC_STATUS_LED_PRIORITY,
-        TX_NO_TIME_SLICE,
-        TX_AUTO_START);
+UINT linc_threads_create(void)
+{
+    const UINT status = tx_thread_create(&linc_status_led_thread, "LINC Status LED", linc_status_led_thread_entry, 0,
+                                         linc_status_led_stack, sizeof(linc_status_led_stack), LINC_STATUS_LED_PRIORITY,
+                                         LINC_STATUS_LED_PRIORITY, TX_NO_TIME_SLICE, TX_AUTO_START);
     return status;
 }
